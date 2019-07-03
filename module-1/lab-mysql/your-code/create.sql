f1f10d9146866806286aaee7926d95a488509e6b
alinedebenath <aline.debenath@gmail.com>
2019-07-03 19:50:55 +0200

    finished

--- module-1/lab-mysql/your-code/create.sql
+++ module-1/lab-mysql/your-code/create.sql
@@ -1,5 +1,5 @@
 USE lab_mysql;
-CREATE TABLE objet (type VARCHAR(20), titre TEXT, INSEE INT, commune VARCHAR(20));
+CREATE TABLE objet (type VARCHAR(20), titre VARCHAR(250), INSEE_id INT, commune VARCHAR(20));
 CREATE TABLE geo (commune VARCHAR(50), departement INT, region VARCHAR(50));
-CREATE TABLE auteurs (name VARCHAR(250));
-CREATE TABLE reservations (nbr_de_reservations INT);
+CREATE TABLE auteurs (titre VARCHAR(250), name VARCHAR(250));
+CREATE TABLE reservations (titre VARCHAR(250), INSEE_id INT, nbr_de_reservations INT);
